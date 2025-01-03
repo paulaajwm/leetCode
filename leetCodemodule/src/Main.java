090311c718adf122c8c87a97f96b6c07f6a0c9e7
programador2 <paula.wmarch@alumnos.upm.es>
2025-01-03 15:08:32 +0100

    exercises done on january 2nd

--- leetCodemodule/src/Main.java
+++ leetCodemodule/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        /*int i = exercise0201.maxScore("011101");
-        System.out.println(i);*/
+        int i = exercise0201.maxScore("011101");
+        System.out.println(i);
 
         String[] words = {"aba", "bcb", "ece", "aa", "e"};
         int[][] queries = {{0, 2}, {1, 4}, {1, 1}};
