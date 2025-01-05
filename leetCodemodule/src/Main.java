77c74882956c4137c1c2498840e5ba702c87d308
programador2 <paula.wmarch@alumnos.upm.es>
2025-01-05 11:48:52 +0100

    exercises done on january 4th

--- leetCodemodule/src/Main.java
+++ leetCodemodule/src/Main.java
@@ -2,21 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
        /* //1
-        int i = exercise0201.maxScore("011101");
-        System.out.println(i);
+        int e1 = exercise0201.maxScore("011101");
+        System.out.println(e1);
 
         //2
         String[] words = {"aba", "bcb", "ece", "aa", "e"};
         int[][] queries = {{0, 2}, {1, 4}, {1, 1}};
-        int[] x = exercise0201.vowelStrings(words, queries);
-        for (int num : x) {
+        int[] e2 = exercise0201.vowelStrings(words, queries);
+        for (int num : e2) {
             System.out.println(num);
-        }*/
+        }
 
         //3
         int[] nums ={2,3,1,0};
-        int y = exercises0301.waysToSplitArray(nums);
-        System.out.println(y);
+        int e3 = exercises0301.waysToSplitArray(nums);
+        System.out.println(e3);*/
 
+        //4
+        int e4 = exercise0401.countPalindromicSubsequence("aabca");
+        System.out.println(e4);
     }
 }
